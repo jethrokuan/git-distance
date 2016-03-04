74d91e972420fb3d85f2ed852b398c7ec7d52bae
Jethro Kuan <jethrokuan95@gmail.com>
2016-03-05 01:14:40 +0800

    Add check for origin remote

--- functions/git_distance.fish
+++ functions/git_distance.fish
@@ -1,9 +1,11 @@
 function git_distance -d "Check how far in front/behind a branch is from origin master"
-  set -l branch (git_branch_name)
-  set -l output
+  if not test -z (git remote | grep "origin")
+    set -l branch (git_branch_name)
+    set -l output
 
-  set output $output (echo -n -s "←" (git rev-list --left-only --count origin/master...HEAD 2> /dev/null))
-  set output $output (echo -n -s  (git rev-list --right-only --count origin/master...HEAD 2> /dev/null) "→")
+    set output $output (echo -n -s " ←" (git rev-list --left-only --count origin/master...HEAD 2> /dev/null))
+    set output $output (echo -n -s  (git rev-list --right-only --count origin/master...HEAD 2> /dev/null) "→")
 
-  echo $output
+    echo $output
+  end
 end
