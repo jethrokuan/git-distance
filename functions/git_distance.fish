1af62f98e125c91700439aca3e94a3126f87975d
Jethro Kuan <jethrokuan95@gmail.com>
2016-03-16 17:29:51 +0800

    Fixed missing args at index 2

--- functions/git_distance.fish
+++ functions/git_distance.fish
@@ -1,11 +1,6 @@
 function git_distance -d "Check how far in front/behind a branch is from origin master"
-  if test ! -z (git remote | grep "origin")
-    set -l git_right_stat (
-    command git rev-list --left-right --count 'HEAD...@{upstream}' ^ /dev/null | awk '
-    $1 > 0 { printf "%s∧", $1 }
-    $2 > 0 { printf "%s∨", $2 }
-    ')
-    
-    echo -n -s $git_right_stat
-  end
+  command git rev-list --left-right --count 'HEAD...@{upstream}' ^ /dev/null | awk '
+  $1 > 0 { printf "%s∧", $1 }
+  $2 > 0 { printf "%s∨", $2 }
+  ' 
 end
