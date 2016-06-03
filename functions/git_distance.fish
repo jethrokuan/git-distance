128b8a75c39812725843c214530a76b3c68bda0e
Jethro Kuan <jethrokuan95@gmail.com>
2016-06-03 22:12:54 +0800

    Update

--- functions/git_distance.fish
+++ functions/git_distance.fish
@@ -1,5 +1,5 @@
 function git_distance
-  command git rev-list --left-right --count '@{upstream}...HEAD' ^ /dev/null | command awk '
+  command git rev-list --left-right --count 'HEAD...@{upstream}' ^ /dev/null | command awk '
   $1 > 0 { printf "%s+", $1}
   $2 > 0 { printf "%s-", $2}
   '
