8a87b4013e344b118e785688683504d25c20927a
Jethro Kuan <jethrokuan95@gmail.com>
2016-06-03 22:09:41 +0800

    Update

--- functions/git_distance.fish
+++ functions/git_distance.fish
@@ -1,6 +1,6 @@
-function git_distance -d "Check how far in front/behind a branch is from origin master"
-  command git rev-list --left-right --count 'HEAD...@{upstream}' ^ /dev/null | awk '
-  $1 > 0 { printf "%s∧", $1 }
-  $2 > 0 { printf "%s∨", $2 }
-  ' 
+function git_distance
+  command git rev-list --left-right --count '@{upstream}...HEAD' ^ /dev/null | command awk '
+  $1 > 0 { printf "%s+", $1}
+  $2 > 0 { printf "%s-", $2}
+  '
 end
