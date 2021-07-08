e5a5b45d57743ce6e4360de19df292fae86d5cf7
Herbert Ilhan Tanujaya <mail@herbert.id>
2021-07-08 11:11:52 +0800

    Update git_distance.fish

--- functions/git_distance.fish
+++ functions/git_distance.fish
@@ -1,5 +1,5 @@
 function git_distance
-  command git rev-list --left-right --count 'HEAD...@{upstream}' | command awk '
+  command git rev-list --left-right --count 'HEAD...@{upstream}' 2> /dev/null | command awk '
   $1 > 0 { printf "%s+", $1}
   $2 > 0 { printf "%s-", $2}
   '
